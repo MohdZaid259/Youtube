03a9911c115f0aaba548259ae92b9ccb32a792bb
Mohd Zaid <[razvizaid259@gmail.com]>
2025-03-03 13:03:31 +0530

    server problem fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import connectDB from "./db/index.js";
 import app from './app.js'
 import serverless from 'serverless-http';
 
-let handler;
+let handler = serverless(app); 
 // const port = process.env.PORT || 8000;
 
 connectDB()
@@ -11,10 +11,10 @@ connectDB()
     // app.listen(port, () => {
     //   console.log(`Server is running on http://localhost:${port}`);
     // })
-    handler = serverless(app); 
+    console.log('server running')
   })
   .catch((err)=>{
     console.log('dbConnection failed ',err)
   })
 
-export {handler}
+export default handler 
