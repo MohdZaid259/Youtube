2c7e8354cad4122023c27f76c0e897245343155a
Mohd Zaid <[razvizaid259@gmail.com]>
2025-03-04 16:04:30 +0530

    server problem fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import 'dotenv/config.js'
 import connectDB from "./db/index.js";
 import app from './app.js'
-import serverless from 'serverless-http';
 
-let handler = serverless(app); 
-// const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 connectDB()
   .then(()=>{
-    // app.listen(port, () => {
-    //   console.log(`Server is running on http://localhost:${port}`);
-    // })
-    console.log('server running')
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    })
   })
   .catch((err)=>{
     console.log('dbConnection failed ',err)
   })
-
-export default handler 
